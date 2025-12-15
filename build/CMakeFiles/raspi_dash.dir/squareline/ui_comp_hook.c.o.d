7939138f8fb8fb1bff7e8df6b5df19bd2255362f
pi1 <sure-fs@pi1>
2025-12-15 12:20:38 +0000

    removed unnecessary files

--- build/CMakeFiles/raspi_dash.dir/squareline/ui_comp_hook.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/raspi_dash.dir/squareline/ui_comp_hook.c.o: \
- /home/sure-fs/raspi-fsdash/squareline/ui_comp_hook.c \
- /usr/include/stdc-predef.h
--- third_party/CMakeFiles/raspi_dash.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for raspi_dash.
-# This may be replaced when dependencies are built.
